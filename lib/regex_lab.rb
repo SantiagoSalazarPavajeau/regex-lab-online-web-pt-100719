0830238d444dc7e0e6de12f18b387eaaeb955582
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-19 18:58:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,11 +16,16 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+[\.!?]$/) ? true : false
-
+  if text.match(/^[A-Z].+[\.!?]$/) 
+    return true 
+  else 
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-  phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
-
+  if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+    return true
+  else
+    false
 end
